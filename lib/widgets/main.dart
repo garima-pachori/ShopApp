bf6ef37d94a440e58375f46f9f9ae370f84dbee1
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-02-25 21:57:07 +0530

    initial commit

---
+++ lib/widgets/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(       
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Lets\'s build a shop!'),
+        ),
+    );
+  }
+}
