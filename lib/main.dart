654dc26d484e0d9f877b9d6052b55bbdd9c0b3f3
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-03-07 18:22:07 +0530

    working with multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
 import './Providers/Products.dart';
-import 'package:provider/provider.dart';
+import './Providers/cart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +15,21 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    
-    return ChangeNotifierProvider(
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider
+        (
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider
+        (
+          create: (ctx) => Cart(),
+        ),
+    ], 
       //The ChangeNotifierProvider widget is part of the provider package, 
       //which is a state management solution that provides a convenient 
       //way to share data between widgets in a Flutter app
-      create: (ctx) => Products(),
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
