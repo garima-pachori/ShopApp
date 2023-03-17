50d5c6d86bb3fa22d507f6fc070d3617197abcab
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-03-17 21:17:07 +0530

    .

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -16,28 +16,49 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          tileColor: Color.fromARGB(255, 171, 241, 118),
-          leading: CircleAvatar(
-            radius: 50.0,
-            child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: FittedBox(child: Text('\$$price')),
-          )),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price*quantity)}'),
-          trailing: CircleAvatar(
-            child: Text('$quantity x')
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20),
+          margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
           ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4
         ),
-        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            tileColor: const Color.fromARGB(255, 171, 241, 118),
+            leading: CircleAvatar(
+              radius: 50.0,
+              child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: FittedBox(child: Text('\$$price')),
+            )),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price*quantity)}'),
+            trailing: CircleAvatar(
+              child: Text('$quantity x')
+            ),
+          ),
+          ),
+      ),
     );
   }
 }
